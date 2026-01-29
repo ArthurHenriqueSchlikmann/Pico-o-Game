5f91d4a1d99b1c03ffcfb8f3d0c4be4db52880eb
ArthurHenriqueSchlikmann <arthursh@outlook.com.br>
2026-01-28 22:44:26 -0300

    header vga started

---
+++ vga/vga.c
@@ -0,0 +1,2 @@
+#include <stdio.h>
+#include "vga.h"
---
+++ vga/vga.h
@@ -0,0 +1,11 @@
+#ifndef VGA_H
+#define VGA_H
+#define redpin 23
+#define bluepin 24
+#define greenpin 25
+
+void draw();
+void setfb();
+void cleanfb();
+
+#endif
